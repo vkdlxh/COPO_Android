c7a1670b197d82b15a98cb6ff6d9da719e508d2a
vkdlxh <jeonsangjun@archive-asia.co.jp>
2020-06-23 22:27:40 +0900

    Repository仮実装

--- app/src/main/java/jp/co/archive/copo/data/repository/FirestoreRepository.kt
+++ app/src/main/java/jp/co/archive/copo/data/repository/FirestoreRepository.kt
@@ -1,30 +1,30 @@
 package jp.co.archive.copo.data.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
+import jp.co.archive.copo.data.model.Result
 import jp.co.archive.copo.data.model.Room
 
 class FirestoreRepository {
 
     private val db = FirebaseFirestore.getInstance()
 
-    fun createRoom(room: Room) {
-        db.collection("rooms").add(room).addOnCompleteListener {
-            if (it.isSuccessful) {
-
-            } else {
-                // TODO: 失敗処理
+    fun createRoom(room: Room, response: Result<Void?>) {
+        db.collection("rooms")
+            .add(room)
+            .addOnSuccessListener {
+                response.onSuccess(null)
+            }.addOnFailureListener {
+                response.onFailure(it)
             }
-        }
     }
 
-    fun getRoomList(adminId: String) {
-        db.collection("rooms").whereEqualTo("adminId", adminId).get().addOnCompleteListener {
-            if (it.isSuccessful) {
-                val list = it.result?.toObjects(Room::class.java)
-
-            } else {
-                // TODO: 失敗処理
+    fun getRoomList(adminId: String, response: Result<List<Room>>) {
+        db.collection("rooms").whereEqualTo("adminId", adminId)
+            .get()
+            .addOnSuccessListener {
+                response.onSuccess(it.toObjects(Room::class.java))
+            }.addOnFailureListener {
+                response.onFailure(it)
             }
-        }
     }
 }
--- app/src/main/java/jp/co/archive/copo/ui/main/MainViewModel.kt
+++ app/src/main/java/jp/co/archive/copo/ui/main/MainViewModel.kt
@@ -1,8 +1,12 @@
 package jp.co.archive.copo.ui.main
 
 import androidx.lifecycle.ViewModel
+import jp.co.archive.copo.data.model.Result
+import jp.co.archive.copo.data.model.Room
 import jp.co.archive.copo.data.repository.AuthRepository
 import jp.co.archive.copo.data.repository.FirestoreRepository
+import java.lang.Exception
+import java.util.*
 
 class MainViewModel(
     private val authRepository: AuthRepository,
@@ -11,6 +15,20 @@ class MainViewModel(
 
     val user = authRepository.user
     val progressBar = authRepository.progressBar
+
+    fun createRoom(title: String, description: String) {
+        val room = Room("", title, description, Date())
+        firestoreRepository.createRoom(room, object : Result<Void?> {
+            override fun onSuccess(response: Void?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(e: Exception) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
 
     fun logout() {
         authRepository.logout()
